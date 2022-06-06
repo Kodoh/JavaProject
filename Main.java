0937da243d617911f79dcc047d4e29c110952164
Jake <jake1132569@gmail.com>
2022-06-06 21:42:29 +0100

    Start of game

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i<6; i++) {
-            if (i % 2 == 0) {
-            System.out.println("Hello World!");
-            }
-        }
+        Scanner read = new Scanner(System.in);
+        System.out.println("Please enter your name - ");
+        String name = read.nextLine();
+        System.out.println("Please enter your age - ");
+        int age = read.nextInt();
+        read.close();
+        Player p1 = new Player(name,age);
+        System.out.println("Your name is " + p1.getName());
     }
 }
 
