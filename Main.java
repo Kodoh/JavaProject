8d8b26821813a39154f393d507a7062742e1188f
Jake <jake1132569@gmail.com>
2022-06-06 21:10:46 +0100

    Second Change

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i<6; i++) {
+            if (i % 2 == 0) {
             System.out.println("Hello World!");
+            }
         }
     }
 }
